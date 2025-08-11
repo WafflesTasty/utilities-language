f3a40081862e9bc297eb4ca574a317202311576e
WafflesTasty <zenothedefiant@gmail.com>
2025-08-11 12:58:41 +0200

    Moved Pair to Collections library, update 1.1.

--- src/waffles/utils/lang/tokens/PairToken.java
+++ src/waffles/utils/lang/tokens/PairToken.java
@@ -2,7 +2,7 @@ package waffles.utils.lang.tokens;
 
 import waffles.utils.lang.tokens.format.Format;
 import waffles.utils.lang.tokens.format.PairFormat;
-import waffles.utils.lang.utilities.Pair;
+import waffles.utils.sets.utilities.Pair;
 
 /**
  * A {@code PairToken} defines a {@code Token} consisting of a key-value {@code Pair}.
